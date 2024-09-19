70585cc6f4d4f8b135b5c5177378bc8af1f80fd6
CNalls <cristiannalls@gmail.com>
2024-09-19 16:06:15 -0500

    Corridor Edits and ColliderForWalls

--- Assets/Resources/Scripts/ProcGenAlg.cs
+++ Assets/Resources/Scripts/ProcGenAlg.cs
@@ -22,6 +22,22 @@ public static class ProcGenAlg
         return path;
     }
 
+    public static List<Vector2Int> RandomWalkCorridor(Vector2Int startPosition, int corridorLength)
+    {
+        List<Vector2Int> corridor = new List<Vector2Int>();
+        var direction = Direction2D.GetRandomCardinalDirection();
+        var currentPosition = startPosition;
+        corridor.Add(currentPosition);
+
+        for (int i = 0; i < corridorLength; i++)
+        {
+            currentPosition += direction;
+            corridor.Add(currentPosition);             
+        } 
+
+        return corridor;
+    }
+
 }
 
 public static class Direction2D
@@ -47,18 +63,19 @@ public static class Direction2D
     }
 }
 
-//public static class Direction2D
-//{
-    //public static List<Vector2Int> cardinalDirectionList = new List<Vector2Int>
-    //{
-        //new Vector2Int(0, 1), //UP
-        //new Vector2Int(1, 0), //RIGHT
-        //new Vector2Int(0, -1), //DOWN
-        //new Vector2Int(-1, 0), //LEFT
-    //};
+/**public static class Direction2D
+{
+    public static List<Vector2Int> cardinalDirectionList = new List<Vector2Int>
+    {
+        new Vector2Int(0, 1), //UP
+        new Vector2Int(1, 0), //RIGHT
+        new Vector2Int(0, -1), //DOWN
+        new Vector2Int(-1, 0), //LEFT
+    };
 
-    //public static Vector2Int GetRandomCardinalDirection()
-    //{
-        //return cardinalDirectionList[Random.Range(0, cardinalDirectionList.Count)];
-    //}
-//}
+    public static Vector2Int GetRandomCardinalDirection()
+    {
+        return cardinalDirectionList[Random.Range(0, cardinalDirectionList.Count)];
+    }
+}
+**/
