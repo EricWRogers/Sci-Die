80aaf9b32dcdc860e20613e7ae69e27ca8dd6bdc
ItsCrasyGamer_YT <potatoe0201@gmail.com>
2024-09-19 16:57:46 -0500

    Implementing gun rotation (kinda)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
         moveDirection = move.action.ReadValue<Vector2>();
+        
         if (Input.GetKeyDown(KeyCode.Space) && canDash)
         {
             StartCoroutine(Dash());
