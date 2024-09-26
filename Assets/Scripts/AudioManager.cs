a9196a9db66c418c799979c3cd6459d02d3c2c7d
DylanGHilliard <ghilliard69420@gmail.com>
2024-09-26 16:53:46 -0500

    Added potiental shotgun audio

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,14 +1,30 @@
 using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using JetBrains.Annotations;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
+    public static AudioManager Instance;
 
 
     public void Awake()
     {
+        //Keeps AudioMangaer between Scenes
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this);
+
+        // Creates sources for each audio clip
+
         foreach (Sound s in sounds)
         {
             s.audioSource = gameObject.AddComponent<AudioSource>();
@@ -20,7 +36,7 @@ public class AudioManager : MonoBehaviour
 
         }
     }
-
+    // Plays the sound you called and named
     public void PlaySound(string name)
     {
         Sound s =  Array.Find(sounds, s => s.name == name);
