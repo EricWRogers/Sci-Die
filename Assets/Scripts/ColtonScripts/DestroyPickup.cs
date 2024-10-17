693df03e2d8e9f9d461729d86ff674003eac022b
Colton Ward <wardc0714@outlook.com>
2024-10-17 16:22:28 -0500

    Weapon Pickups Working
    Still have to set gun on pickup, though

--- Assets/Scripts/ColtonScripts/DestroyPickup.cs
+++ Assets/Scripts/ColtonScripts/DestroyPickup.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class DestroyPickup : MonoBehaviour
 {
-    void OnTriggerStay2D(Collider2D other){
-        if(Input.GetKeyDown(KeyCode.E)){
-            Debug.Log("Pickup");
+    bool playerColliding = false;
+    void Update()
+    {
+        if(playerColliding && (Input.GetKeyDown(KeyCode.E))){
             Destroy(gameObject);
         }
     }
+    void OnTriggerStay2D(Collider2D other){
+        playerColliding = true;
+    }
+    
 }
