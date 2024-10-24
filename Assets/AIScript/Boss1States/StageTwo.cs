024e83c2c61196197c4ce8081241078554b6bd4b
Garrett Shipp <garwad9@gmail.com>
2024-10-24 16:51:12 -0500

    boss one stage two in progress

--- Assets/AIScript/Boss1States/StageTwo.cs
+++ Assets/AIScript/Boss1States/StageTwo.cs
@@ -3,7 +3,7 @@ using SuperPupSystems.StateMachine;
 using System;
 
 [System.Serializable]
-public class StageOne : SimpleState
+public class StageTwo : SimpleState
 {
     public override void OnStart()
     {
@@ -14,15 +14,18 @@ public class StageOne : SimpleState
     public override void UpdateState(float _dt)
     {
         base.UpdateState(_dt);
+        if (((BossStateMachine)stateMachine).centerAttacking)
+        {
+            ((BossStateMachine)stateMachine).Lasers.SetActive(true);
+        }
+        else
+        {
+            ((BossStateMachine)stateMachine).Lasers.SetActive(false);
+        }
         ((BossStateMachine)stateMachine).RepeatMov(3);
         if (((BossStateMachine)stateMachine).timer.timeLeft <= 0)
         {
             ((BossStateMachine)stateMachine).CenterAttack();
-        }
-        if (((BossStateMachine)stateMachine).halfHealth)
-        {
-
-            ((BossStateMachine)stateMachine).ChangeState(nameof(StageTwo));
         }
     }
 
