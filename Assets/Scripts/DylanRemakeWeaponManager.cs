0b8b54bfa9ef14c7231f2970c862151c816f4584
DylanGHilliard <ghilliard69420@gmail.com>
2024-11-06 17:56:07 -0600

    testing drone on computer

---
+++ Assets/Scripts/DylanRemakeWeaponManager.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using SuperPupSystems.Helper;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DylanRemakeWeaponManager : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public GameObject railgunReady;
+
+    public Transform weapon;
+
+    private PlayerControls controls;
+
+    public float fireRate;
+    
+    private float time = 0.0f;
+
+    public string activeGun;
+
+    private float destroyTimer;
+
+    private float railgunCharge;
+
+    public bool isCharging = true;
+    //Dylan Vars
+    public Transform attackPoint;
+    public int attackDmg;
+    public float m_angle;
+    public LayerMask enemyLayers;
+
+    public WeaponAsset defaultWeaponAsset;
+
+    public DylanRemakeWeaponManager(){
+
+    }
+    void Awake()
+        {
+            UpdateWeapon(defaultWeaponAsset);
+            controls = new PlayerControls();
+            controls.Player.Dash.performed += ctx => Update();
+        }
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    
+
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        time += Time.deltaTime;
+        float timeToNextFire = 1/fireRate;
+        if (isCharging){
+            railgunCharge += Time.deltaTime;
+            if(railgunCharge >= 5){
+                isCharging = false;
+                railgunReady.SetActive(true);
+            }
+        }
+
+        bool fireInput = Input.GetKeyDown(KeyCode.Mouse0) || controls.Player.Fire.triggered;
+
+        if ((activeGun == "RocketLauncher" || activeGun == "Shotgun" || activeGun == "Railgun") && fireInput){
+            if (activeGun == "RocketLauncher"){
+                if(time >= timeToNextFire){
+                    Instantiate(bullet, weapon.position, weapon.rotation);
+                    time = 0;
+                }
+            }
+            
+            if (activeGun == "Shotgun"){
+                if(time >= timeToNextFire){
+                    Instantiate(bullet, weapon.position, weapon.rotation);
+                    time = 0;
+                }
+            }
+
+            if (activeGun == "Railgun" && railgunCharge >= 5){
+                GameObject go = Instantiate(bullet, weapon.position, weapon.rotation);
+                time = 0;
+                railgunCharge = 0;
+                isCharging = true;
+                railgunReady.SetActive(false);
+                destroyTimer += 1;
+                if(destroyTimer == 3.0f){
+                    Destroy(go);
+                    destroyTimer = 0;
+                }
+            }
+        }
+
+ 
+
+        if ((activeGun == "Pistol" || activeGun == "MachineGun") && (Input.GetKey(KeyCode.Mouse0) || controls.Player.Fire.ReadValue<float>() > 0) && time >= 0){
+            if(activeGun == "Pistol"){
+                if(time >= timeToNextFire){
+                    Instantiate(bullet, weapon.position, weapon.rotation);
+                    time = 0;
+                }
+            }
+            if(activeGun == "MachineGun"){
+                if(time >= timeToNextFire){
+                    Instantiate(bullet, weapon.position, weapon.rotation);
+                    time = 0;
+                }
+            }
+                
+        }
+        if (activeGun == "ScissorDrone" && fireInput && timeToNextFire < Time.time)
+        {
+            Debug.Log("ScissorDroneIf");
+            Invoke(nameof(ScissorAttack), .5f);
+            time = 0;
+        }
+        
+    }
+    public void UpdateWeapon(WeaponAsset m_weaponAsset)
+    {
+        bullet = m_weaponAsset.bullet;
+        activeGun = m_weaponAsset.activeGun;
+        fireRate = m_weaponAsset.fireRate;
+        attackDmg = m_weaponAsset.attackDmg;
+    }
+    private void ScissorAttack()
+    {
+              Debug.Log("ScissorDronedamage");
+            
+            Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPoint.position, new Vector2(2,2), m_angle, enemyLayers);
+
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<Health>().TakeDamage(attackDmg);
+            }
+        
+    }
+}
