23df04eade7e1c564d89102f6ceb8c5b51ac0891
Garrett Shipp <garwad9@gmail.com>
2024-11-21 12:33:20 -0600

    scrap manager

--- Assets/scrapManager.cs
+++ Assets/scrapManager.cs
@@ -1,22 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class scrapManager : MonoBehaviour
+public class ScrapManager : MonoBehaviour
 {
-    public int scrap;
-    public TMP_Text scrapText;
-
-    // Start is called before the first frame update
+    public int scrapCount;
+    public TMP_Text ScrapTXT;
+    public ScrapManager sm;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        scrapText.text = scrap.ToString();
+        ScrapTXT.text = "Scrap Count: " + scrapCount.ToString();
     }
 }
